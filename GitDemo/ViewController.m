379944f8b2db259f15ab62ebc1db975a433aa021
macadmin <macadmin@Administrators-iMac-17.local>
2015-01-21 11:20:29 -0500

    nk

--- GitDemo/ViewController.m
+++ GitDemo/ViewController.m
@@ -19,7 +19,7 @@
     [super viewDidLoad];
     
     // *************  Neelam  ****************
-    
+    //baljit
     //yao Lu
     //testsdfsdfsf
     // Do any additional setup after loading the view, typically from a nib.
